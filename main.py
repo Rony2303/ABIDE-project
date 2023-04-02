e26c3a3212404e1a8923dacf50f5b3f55d56ba93
Rony2303 <ronyzerkavod@gmail.com>
2023-04-02 11:17:30 +0300

    with ABIDE projection on HCP

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from sklearn.decomposition import PCA
+from os.path import join as opj
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import os
+import csv
 
 
 # create data
+
+
 def read_ids_from_metadata(path: str):
     metadata = pd.read_csv(path)
     asd_ids = metadata.loc[metadata['DX_GROUP'] == 1, 'SUB_ID']
@@ -14,46 +18,54 @@ def read_ids_from_metadata(path: str):
 
 
 def join_subjects_to_groups(path: str, asd_ids: list, control_ids: list):
-    asd_group, control_group = [], []
+    asd_group, control_group, updated_asd_id, updated_control_id = [], [], [], []
     for asd_id in asd_ids:
         try:
             curr_features = np.load(path + "00" + str(asd_id) + ".npy")
         except FileNotFoundError:
             continue
         asd_group.append(curr_features)
+        updated_asd_id.append(asd_id)
     for control_id in control_ids:
         try:
             curr_features = np.load(path + "00" + str(control_id) + ".npy")
         except FileNotFoundError:
             continue
         control_group.append(curr_features)
-    return np.array(asd_group), np.array(control_group)
+        updated_control_id.append(control_id)
+    df_ASD_id = pd.DataFrame(updated_asd_id)
+    df_control_id = pd.DataFrame(updated_control_id)
+    df_ASD_id.to_csv('/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/ASD_ids.csv')
+    df_control_id.to_csv('/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/Control_ids.csv')
+    asd_features = np.array(asd_group)
+    control_features = np.array(control_group)
+    np.savetxt("/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/asd_features.csv", asd_features,
+               delimiter=",")
+    np.savetxt("/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/control_features.csv", control_features,
+               delimiter=",")
 
 
 def create_features_groups(path_to_features, path_to_metadata, saved_dir):
     asd_ids, control_ids = read_ids_from_metadata(path_to_metadata)
-    asd_group, control_group = join_subjects_to_groups(path_to_features, asd_ids, control_ids)
-    np.savez(saved_dir, asd_group=asd_group, control_group=control_group)
-    return asd_group, control_group
+    join_subjects_to_groups(path_to_features, asd_ids, control_ids)
 
 
-def create_data_graph(results_dir, hcp_data, asd_data=None, control_data=None):
+def create_data_graph(results_dir, plot_name, hcp_data, asd_data=None, control_data=None):
     ax = plt.gca()
-    ax.scatter(hcp_data[:, 0], hcp_data[:, 1], s=1.5, c='black', alpha=0.7)
-    plt_name = "HCP_data"
+    ax.scatter(hcp_data[:, 0], hcp_data[:, 1], s=1.5, c='dimgrey', alpha=0.7, label='HCP Data')
     if asd_data is not None:
-        ax.scatter(asd_data[:, 0], asd_data[:, 1], s=1.5, c='red', alpha=0.7)
-        plt_name = "ASD_vs_HCP.png"
+        ax.scatter(asd_data[:, 0], asd_data[:, 1], s=1.5, c='indianred', alpha=0.7, label='ASD Data')
     if control_data is not None:
-        ax.scatter(control_data[:, 0], control_data[:, 1], s=1.5, c='blue', alpha=0.7)
-        plt_name = "control_vs_HCP.png"
-        if asd_data is not None:
-            plt_name = "ASD_and_control_vs_HCP.png"
+        ax.scatter(control_data[:, 0], control_data[:, 1], s=1.5, c='mediumaquamarine', alpha=0.7, label='ASD Control Data')
+    legend = ax.legend(loc='upper right', shadow=False, fontsize='x-small')
+    legend.get_frame().set_facecolor('thistle')
     plt.xlabel("PC1")
     plt.ylabel("PC2")
-    plt.xlim(-16, 16)
-    plt.ylim(-8, 12)
-    plt.savefig(opj(results_dir, plt_name))
+    plt.xlim(-6, 6)
+    plt.ylim(-6, 6)
+    plt.savefig(opj(results_dir, plot_name))
+    plt.legend()
+    plt.clf()
 
 
 # A
@@ -77,9 +89,28 @@ def abide_vs_hcp_plot(results_dir, hcp_features: np.array, asd_features: np.arra
     hcp_pca = pca.fit_transform(hcp_features)
     asd_pca = pca.transform(asd_features)
     control_pca = pca.transform(control_features)
-    create_data_graph(results_dir, hcp_pca, asd_pca, control_pca)
-    create_data_graph(results_dir, hcp_pca, control_pca)
-    create_data_graph(results_dir, hcp_pca, asd_pca)
+    plot_name = "HCP_vs_ASD_and_CONTROL.png"
+    create_data_graph(results_dir, plot_name, hcp_pca, asd_pca, control_pca)
+    plot_name = "HCP_vs_CONTROL.png"
+    create_data_graph(results_dir, plot_name, hcp_pca, None, control_pca)
+    plot_name = "HCP_vs_ASD.png"
+    create_data_graph(results_dir, plot_name, hcp_pca, asd_pca, None)
+
+    # Explained Variance
+
+    # asd_control_on_hcp_explained_var = np.var(hcp_pca.transform(control_features), axis=0) / np.var(control_features,
+    #                                                                                 axis=0).sum()
+    # ax = plt.subplots(figsize=(7, 5))
+    #
+    # hcp_bar = ax.bar(x=np.arange(2), height=hcp_pca.explained_variance_ratio_, width=0.4)
+    # asd_control_bar = ax.bar(x=np.arange(2) + 0.4, height=asd_control_on_hcp_explained_var, width=0.4)
+    # ax.set_xticks(np.arange(2) + 0.2)
+    # ax.set_xticklabels(['PC1', 'PC2'])
+    # ax.set(ylabel='Explained Variance ratio')
+    # ax.legend((hcp_bar[0], asd_control_bar[0]), ('HCP', 'ASD CONTROL'))
+    #
+    # ax.set_title('Explained Variance ratio of HCP-ASD CONTROL\non PC1, PC2 of HCP')
+    # plt.savefig(opj(results_dir, "explained_variance_HCP_ASD-CONTROL.png"))
 
 
 # from here those are all statistic tests to justify the use of HCP Pareto Front
@@ -120,9 +151,15 @@ def projections_random_vector_from_hcp():
 
 
 if __name__ == '__main__':
-    path_to_features = "/sci/labs/uvhart/ronyzerkavod/code/PreProcessing/outputs/abide_features/"
+    path_to_features = "/sci/labs/uvhart/ronyzerkavod/data/ABIDE/preprocessed_data/"
     path_to_metadata = "/sci/labs/uvhart/ronyzerkavod/code/PreProcessing/Phenotypic_V1_0b_preprocessed.csv"
-    results_dir = "/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/groups.npz"
+    results_groups_path = "/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/groups.npz"
+    results_dir = "/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/plots/"
     hcp_features = pd.read_csv("/sci/labs/uvhart/ronyzerkavod/code/ABIDE_project/hcp_features.csv")
-    asd_group_f, control_group_f = create_features_groups(path_to_features, path_to_metadata, results_dir)
-    #abide_vs_hcp_plot(results_dir, hcp_features, asd_group_f, control_group_f, 2)
+    # only for initiation - init groups (ASD & control)
+    # create_features_groups(path_to_features, path_to_metadata, results_dir)
+    features_ASD = np.genfromtxt('/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/asd_features.csv',
+                                 delimiter=',')
+    features_control = np.genfromtxt(
+        '/sci/labs/uvhart/ronyzerkavod/ouputs/ABIDE/asd_control_groups/control_features.csv', delimiter=',')
+    abide_vs_hcp_plot(results_dir, hcp_features, features_ASD, features_control, 2)
